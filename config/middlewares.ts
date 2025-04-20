9021b1d422669f0f891c864b6a8f34597aea6fa7
DilankaSilva <136069673+DilankaSilva@users.noreply.github.com>
2025-04-20 23:20:40 +0530

    Initial Content

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -1,8 +1,29 @@
-export default [
+module.exports = [
   'strapi::logger',
   'strapi::errors',
-  'strapi::security',
-  'strapi::cors',
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'img-src': ["'self'", 'data:', 'blob:', 'localhost:1337', 'res.cloudinary.com'], // Add your domains
+        },
+      },
+    },
+  },
+  {
+    name: 'strapi::cors',
+    config: {
+      origin: [
+        'http://localhost:3000', // Your Next.js frontend
+        'http://localhost:1337', // Your Strapi backend
+      ],
+      methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'HEAD', 'OPTIONS'],
+      headers: ['Content-Type', 'Authorization', 'Origin', 'Accept'],
+      keepHeaderOnError: true,
+    },
+  },
   'strapi::poweredBy',
   'strapi::query',
   'strapi::body',
